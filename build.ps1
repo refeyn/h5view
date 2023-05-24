d201c206fe660ead24891da80f9b9771d34f1da3
matsjoyce <matthew.joyce@refeyn.com>
2023-05-24 16:33:24 +0100

    Make hdf5plugin work deployed

--- build.ps1
+++ build.ps1
@@ -2,4 +2,5 @@
 python -m nuitka h5view `
     --include-package=h5view `
     --onefile --enable-plugin=pyside6 `
+    --user-package-configuration-file=user.nuitka-package.config.yml `
     --include-qt-plugins=sensible --disable-ccache
